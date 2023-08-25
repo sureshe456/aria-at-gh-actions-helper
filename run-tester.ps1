1ba996fdbd775b9bef4d1ba7586716e39a28d014
Mx. Corey Frang <gnarf37@gmail.com>
2023-08-25 16:19:24 -0400

    also look for notepad?

--- run-tester.ps1
+++ run-tester.ps1
@@ -1,15 +1,15 @@
-Start-Job -ScriptBlock { at-driver *>&1 >at-driver.output }
+Start-Job -ScriptBlock { at-driver *>&1 >$env:TEMP\at-driver.output }
 Start-Sleep -Seconds 10
-Start-Job -ScriptBlock { chromedriver --port=4444 --log-level=INFO *>&1 >chromedriver.output }
+Start-Job -ScriptBlock { chromedriver --port=4444 --log-level=INFO *>&1 >$env:TEMP\chromedriver.output }
 Start-Sleep -Seconds 10
 start nvda-portable\2023.1.0.27913\NVDA.exe
 Start-Sleep -Seconds 10
 cd aria-at/build/tests/alert
 Start-Sleep -Seconds 10
 echo "--at-driver.output"
-Get-Content -Path ../../../../at-driver.output -ErrorAction Continue
+Get-Content -Path $env:TEMP\at-driver.output -ErrorAction Continue
 echo "--chromedriver.output"
-Get-Content -Path ../../../../chromedriver.output -ErrorAction Continue
+Get-Content -Path $env:TEMP\chromedriver.output -ErrorAction Continue
 echo "--nvda.log???"
 Get-Content -Path $env:TEMP\nvda.log -ErrorAction Continue
 
@@ -30,14 +30,22 @@ $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
 
 $bmp.Save("D:\a\aria-at-gh-actions-helper\test.png")
 
-$graphics.Dispose()
-$bmp.Dispose()
 
 node ../../../../automation-harness/bin/host.js run-plan --debug --tests-match 'test-01-trigger-alert*.json' '**/*.html'
 
+$graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+
+Start-Process notepad
+
+Start-Sleep -Seconds 10
+
+$bmp.Save("D:\a\aria-at-gh-actions-helper\test2.png")
+$graphics.Dispose()
+$bmp.Dispose()
+
 echo "--at-driver.output"
-Get-Content -Path ../../../../at-driver.output -ErrorAction Continue
+Get-Content -Path $env:TEMP\at-driver.output -ErrorAction Continue
 echo "--chromedriver.output"
-Get-Content -Path ../../../../chromedriver.output -ErrorAction Continue
+Get-Content -Path $env:TEMP\chromedriver.output -ErrorAction Continue
 echo "--nvda.log???"
 Get-Content -Path $env:TEMP\nvda.log -ErrorAction Continue
