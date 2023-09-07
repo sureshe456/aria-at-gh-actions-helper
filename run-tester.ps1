219927977cc0dfc46998bb44875de9ee2fa3246c
Mx. Corey Frang <gnarf37@gmail.com>
2023-09-07 11:34:18 -0400

    store loglocation and hopefully "fix" some problems

--- run-tester.ps1
+++ run-tester.ps1
@@ -1,9 +1,15 @@
-nvda-portable\2022.3.0.26722\NVDA.exe # --debug-logging
+nvda-portable\2022.3.0.26722\NVDA.exe --debug-logging
+$loglocation = $pwd
+
+Write-Output "Log folder $loglocation"
+
 Start-Sleep -Seconds 10
-$atprocess = Start-Job -Init ([ScriptBlock]::Create("Set-Location '$pwd\nvda-at-automation\Server'")) -ScriptBlock { & .\main.exe 2>&1 >..\..\at-driver.log }
+$atprocess = Start-Job -Init ([ScriptBlock]::Create("Set-Location '$pwd\nvda-at-automation\Server'")) -ScriptBlock { & .\main.exe 2>&1 >$loglocation\at-driver.log }
 Start-Sleep -Seconds 10
-$chromeprocess = Start-Job -Init ([ScriptBlock]::Create("Set-Location '$pwd'")) -ScriptBlock { chromedriver --port=4444 --log-level=INFO *>&1 >chromedriver.log }
+$chromeprocess = Start-Job -Init ([ScriptBlock]::Create("Set-Location '$pwd'")) -ScriptBlock { chromedriver --port=4444 --log-level=INFO *>&1 >$loglocation\chromedriver.log }
 Start-Sleep -Seconds 10
+
+
 
 Set-Location aria-at-automation-harness
 
@@ -11,11 +17,11 @@ function Trace-Logs {
   Write-Output "At-Driver job process log:"
   Receive-Job $atprocess
   Write-Output "--at-driver.log"
-  Get-Content -Path ..\at-driver.log -ErrorAction Continue
+  Get-Content -Path $loglocation\at-driver.log -ErrorAction Continue
   Write-Output "chromedriver job process log:"
   Receive-Job $chromeprocess
   Write-Output "--chromedriver.log"
-  Get-Content -Path ..\chromedriver.log -ErrorAction Continue
+  Get-Content -Path $loglocation\chromedriver.log -ErrorAction Continue
   Write-Output "--nvda.log???"
   Get-Content -Path $env:TEMP\nvda.log -ErrorAction Continue
 }
@@ -37,22 +43,23 @@ $graphics = [Drawing.Graphics]::FromImage($bmp)
 
 $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
 
-$bmp.Save("D:\a\aria-at-gh-actions-helper\test.png")
+$bmp.Save("$loglocation\test.png")
 
 
 node bin/host.js  run-plan --plan-workingdir ../aria-at/build/tests/alert "reference/**,test-01-*-nvda.*" --agent-web-driver-url=http://127.0.0.1:4444 --agent-at-driver-url=ws://127.0.0.1:3031 --reference-hostname=127.0.0.1 --debug --agent-debug | Tee-Object -FilePath ..\harness-run.log
 $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
-$bmp.Save("D:\a\aria-at-gh-actions-helper\test2.png")
+$bmp.Save("$loglocation\test2.png")
 
 Start-Process notepad
 
 Start-Sleep -Seconds 10
 $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
-$bmp.Save("D:\a\aria-at-gh-actions-helper\test3.png")
+$bmp.Save("$loglocation\test3.png")
 $graphics.Dispose()
 $bmp.Dispose()
 
+Set-Location ..
 get-process > .\get-process.log
-Copy-Item -Path $env:TEMP\nvda.log -Destination D:\a\aria-at-gh-actions-helper\ -ErrorAction Continue
+Copy-Item -Path $env:TEMP\nvda.log -Destination $loglocation -ErrorAction Continue
 
 Trace-Logs
