46d0ea122ceb3ab67c85b03a477348fe770789ae
Mx. Corey Frang <gnarf37@gmail.com>
2023-09-07 13:07:11 -0400

    cleaning up run-tester.ps1

--- run-tester.ps1
+++ run-tester.ps1
@@ -1,30 +1,17 @@
+Write-Output "Starting NVDA"
 nvda-portable\2022.3.0.26722\NVDA.exe --debug-logging
-$loglocation = $pwd
 Start-Sleep -Seconds 10
-try {
-  Invoke-WebRequest -UseBasicParsing http://localhost:8765/info
-}
-catch {
-  <#Do this if a terminating exception happens#>
-}
 
-try {
-  Invoke-WebRequest -UseBasicParsing http://127.0.0.1:8765/info
-}
-catch {
-  <#Do this if a terminating exception happens#>
-}
-
+$loglocation = $pwd
 Write-Output "Log folder $loglocation"
 
+Write-Output "Starting at-driver"
 $atprocess = Start-Job -Init ([ScriptBlock]::Create("Set-Location '$pwd\nvda-at-automation\Server'")) -ScriptBlock { & .\main.exe 2>&1 >$using:loglocation\at-driver.log }
 Start-Sleep -Seconds 10
+
+Write-Output "Starting chromedriver"
 $chromeprocess = Start-Job -Init ([ScriptBlock]::Create("Set-Location '$pwd'")) -ScriptBlock { chromedriver --port=4444 --log-level=INFO *>&1 >$using:loglocation\chromedriver.log }
 Start-Sleep -Seconds 10
-
-
-
-Set-Location aria-at-automation-harness
 
 function Trace-Logs {
   Write-Output "At-Driver job process log:"
@@ -42,27 +29,29 @@ function Trace-Logs {
 Trace-Logs
 
 Add-Type -AssemblyName System.Windows.Forms,System.Drawing
-
 $screens = [Windows.Forms.Screen]::AllScreens
-
 $top    = ($screens.Bounds.Top    | Measure-Object -Minimum).Minimum
 $left   = ($screens.Bounds.Left   | Measure-Object -Minimum).Minimum
 $width  = ($screens.Bounds.Right  | Measure-Object -Maximum).Maximum
 $height = ($screens.Bounds.Bottom | Measure-Object -Maximum).Maximum
-
 $bounds   = [Drawing.Rectangle]::FromLTRB($left, $top, $width, $height)
 $bmp      = New-Object System.Drawing.Bitmap ([int]$bounds.width), ([int]$bounds.height)
 $graphics = [Drawing.Graphics]::FromImage($bmp)
 
 $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
-
 $bmp.Save("$loglocation\test.png")
 
 
-node bin/host.js  run-plan --plan-workingdir ../aria-at/build/tests/alert "reference/**,test-01-*-nvda.*" --agent-web-driver-url=http://127.0.0.1:4444 --agent-at-driver-url=ws://127.0.0.1:3031 --reference-hostname=127.0.0.1 --debug --agent-debug | Tee-Object -FilePath ..\harness-run.log
+Write-Output "Launching automation-harness host"
+Set-Location aria-at-automation-harness
+
+node bin/host.js  run-plan --plan-workingdir ../aria-at/build/tests/alert "reference/**,test-01-*-nvda.*" --agent-web-driver-url=http://127.0.0.1:4444 --agent-at-driver-url=ws://127.0.0.1:3031 --reference-hostname=127.0.0.1 --debug --agent-debug | Tee-Object -FilePath $loglocation\harness-run.log
+
 $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
 $bmp.Save("$loglocation\test2.png")
 
+
+Write-Output "Opening notepad for good luck (and screenshot purposes)"
 Start-Process notepad
 
 Start-Sleep -Seconds 10
