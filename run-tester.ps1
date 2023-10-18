746442a609ddf946dc6e5ab440bde02a4eb2fce3
Mx. Corey Frang <gnarf37@gmail.com>
2023-10-18 12:07:36 -0400

    Update nvda-chrome to take workingdir/test pattern as inputs

--- run-tester.ps1
+++ run-tester.ps1
@@ -93,7 +93,7 @@ if ($env:RUNNER_DEBUG)
 {
   $hostParams = "--debug"
 }
-node aria-at-automation-harness/bin/host.js  run-plan --plan-workingdir aria-at/build/tests/alert "reference/**,test-*-nvda.*" $hostParams --agent-web-driver-url=http://127.0.0.1:4444 --agent-at-driver-url=ws://127.0.0.1:3031/command --reference-hostname=127.0.0.1 --agent-web-driver-browser=chrome | Tee-Object -FilePath $loglocation\harness-run.log
+node aria-at-automation-harness/bin/host.js  run-plan --plan-workingdir aria-at/build/$env:ARIA_AT_WORK_DIR $env:ARIA_AT_TEST_PATTERN $hostParams --agent-web-driver-url=http://127.0.0.1:4444 --agent-at-driver-url=ws://127.0.0.1:3031/command --reference-hostname=127.0.0.1 --agent-web-driver-browser=chrome | Tee-Object -FilePath $loglocation\harness-run.log
 
 $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
 $bmp.Save("$loglocation\test2.png")
